68dd31fb5dd45d6e208afc54778547277b514697
Daniel Salgado <danielsalgado@unitec.edu>
2016-09-23 18:27:55 -0600

    Ingresa Apuesta

--- main.cpp
+++ main.cpp
@@ -24,104 +24,110 @@ int main(int argc, char* argv[]){
 		cout << i << " " << personas[i]->toString() << endl;
         }
 	cout << endl;
-
-	cout << "Login: " << endl;
-	string id;
-	string nombre;
-	cout << "Ingrese Nombre" << endl;
-	cin >> nombre;
-	cout << "Ingrese Identidad" << endl;
-	cin >> id;
-	for (int i=0; i<personas.size(); i++){
-		if(personas[i]->getNombre() == nombre && personas[i]->getId()== id){
-			if(Administrador *a = dynamic_cast<Administrador*>(personas[i])){
-				int op = 0;
-				while(op != 4){
-					cout << "1. Agregar Mesas: " << endl;
-					cout << "2. Modificar Mesas: " << endl;
-					cout << "3. Eliminar Mesas: " << endl;
-					cin >> op;
-					if(op == 1){
-						int numero;
-						string tipo;
-						string nombre;
-						int edad;
-						string id;
-						string dificultad;
-						double dinero;
-						int** cartas = new int*[14];
-						for(int i =0; i < 14 ;i++){
-							cartas[i]=new int[5];
+	bool valido = false;
+	while(valido == false){
+		cout << endl;
+		cout << "Login: " << endl;
+		string id;
+		string nombre;
+		cout << "Ingrese Nombre" << endl;
+		cin >> nombre;
+		cout << "Ingrese Identidad" << endl;
+		cin >> id;
+		for (int i=0; i<personas.size(); i++){
+			if(personas[i]->getNombre() == nombre && personas[i]->getId()== id){
+				cout << endl;
+				cout << "BIENVENIDO" << endl;
+				valido = true;
+				if(Administrador *a = dynamic_cast<Administrador*>(personas[i])){
+					int op = 0;
+					while(op != 4){
+						cout << "1. Agregar Mesas: " << endl;
+						cout << "2. Modificar Mesas: " << endl;
+						cout << "3. Eliminar Mesas: " << endl;
+						cin >> op;
+						if(op == 1){
+							int numero;
+							string tipo;
+							string nombre;
+							int edad;
+							string id;
+							string dificultad;
+							double dinero;
+							int** cartas = new int*[14];
+							for(int i =0; i < 14 ;i++){
+								cartas[i]=new int[5];
+							}
+							int* palos = new int[53];
+	 						int* valores = new int[53];
+							cout << "Ingrese Numero De Mesa: " << endl;
+							cin >> numero;
+							cout << "Ingrese El Tipo VIP,CLASICA,VIAJERO: " << endl;
+							cin >> tipo;
+							cout << "Ingrese Repartidor" << endl;
+							cout << "Ingrese Nombre: " << endl;
+							cin >> nombre;
+							cout << "Ingrese Edad: " << endl;
+							cin >> edad;
+							cout << "Ingrese Id: " << endl;;
+							cin >> id;
+							cout << "Ingrese Dificultad: " << endl;
+							cin >> dificultad;
+							cout << "Ingrese Dinero: " << endl;
+							cin >> dinero;
+							cout << "Ingrese Jugador: " << endl;
+							string nombre1;
+							int edad1;
+							string id1;
+							string procedencia;
+							string apodo;
+							double monto;
+							cout << "Ingrese Nombre: " << endl;
+							cin >> nombre1;
+							cout << "Ingrese Edad: " << endl;
+							cin >> edad1;
+							cout << "Ingrese Identidad: " << endl;
+							cin >> id1;
+							cout << "Ingrese Procedencia: " << endl;
+							cin >> procedencia;
+							cout << "Ingrese Apodo " << endl;
+							cin >> apodo;
+							cout << "Ingrese Monto: " << endl;
+							cin >> monto;
+							Jugador* jugador = new Jugador(nombre1,edad1,id1,procedencia,apodo,monto);
+							Baraja* baraja = new Baraja(palos,valores,cartas);
+							Repartidor* repartidor = new Repartidor(nombre,edad,id,dificultad,dinero,baraja);
+							mesas.push_back(new Mesa(numero,tipo,repartidor,jugador));
+							cout << "MESA AGREGADA" << endl;
 						}
-						int* palos = new int[53];
- 						int* valores = new int[53];
-						cout << "Ingrese Numero De Mesa: " << endl;
-						cin >> numero;
-						cout << "Ingrese El Tipo VIP,CLASICA,VIAJERO: " << endl;
-						cin >> tipo;
-						cout << "Ingrese Repartidor" << endl;
-						cout << "Ingrese Nombre: " << endl;
-						cin >> nombre;
-						cout << "Ingrese Edad: " << endl;
-						cin >> edad;
-						cout << "Ingrese Id: " << endl;;
-						cin >> id;
-						cout << "Ingrese Dificultad: " << endl;
-						cin >> dificultad;
-						cout << "Ingrese Dinero: " << endl;
-						cin >> dinero;
-						cout << "Ingrese Jugador: " << endl;
-						string nombre1;
-						int edad1;
-						string id1;
-						string procedencia;
-						string apodo;
-						double monto;
-						cout << "Ingrese Nombre: " << endl;
-						cin >> nombre1;
-						cout << "Ingrese Edad: " << endl;
-						cin >> edad1;
-						cout << "Ingrese Identidad: " << endl;
-						cin >> id1;
-						cout << "Ingrese Procedencia: " << endl;
-						cin >> procedencia;
-						cout << "Ingrese Apodo " << endl;
-						cin >> apodo;
-						cout << "Ingrese Monto: " << endl;
-						cin >> monto;
-						Jugador* jugador = new Jugador(nombre1,edad1,id1,procedencia,apodo,monto);
-						Baraja* baraja = new Baraja(palos,valores,cartas);
-						Repartidor* repartidor = new Repartidor(nombre,edad,id,dificultad,dinero,baraja);
-						mesas.push_back(new Mesa(numero,tipo,repartidor,jugador));
-						cout << "MESA AGREGADA" << endl;
-					}
-					if (op == 2){
+						if (op == 2){
 					
-					}
-					if(op == 3){
-						int pos;
-						for (int i=0; i<mesas.size(); i++){
-							cout << i << " " << mesas[i]->toString() << endl;
-                        			}
-						cout << "Ingrese La Posicion A Borrar: " << endl;
-						cin >> pos;
-						mesas.erase(mesas.begin()+pos);
-						cout << "ELIMINADO" << endl;
-					}
-				}				
-			}	
+						}
+						if(op == 3){
+							int pos;
+							for (int i=0; i<mesas.size(); i++){
+								cout << i << " " << mesas[i]->toString() << endl;
+		                			}
+							cout << "Ingrese La Posicion A Borrar: " << endl;
+							cin >> pos;
+							mesas.erase(mesas.begin()+pos);
+							cout << "ELIMINADO" << endl;
+						}
+					}				
+				}
+				if(Jugador *j = dynamic_cast<Jugador*>(personas[i])){
+					valido = true;
+					double apuesta;
+					cout << "Jugar!" << endl;
+					cout << "Ingrese Su Apuesta: " << endl;
+					cin >> apuesta;
+				}	
+			}
+			else{
+				valido = false;
+			}
 		}
 	}
-	/*while(opcion != 3){
-		cout << "1. Agregar Persona" << endl;
-		cout << "2. Login" << endl;
-		cout << "3. Salir" << endl;
-		cin >> opcion;
-		if(opcion == 1){
-			int op = 0;
-			
 
-		}
-	}*/
 	return 0;
 }
